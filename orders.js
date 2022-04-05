ef317f40208af020019e3a943769aa457e1578a3
mbabaziviolet <mbabazijoy549@gmail.com>
2022-04-05 11:21:39 -0800

    userinterface final

--- orders.js
+++ orders.js
@@ -34,3 +34,17 @@ $(document).ready(function(){
     });
 
 });
+
+
+var addItemId = 0;
+function addToCart(item){
+    addItemId += 1;
+    var selectedItem = document.createElement('div');
+    selectedItem.classList.add('cartImg');
+    selectedItem.setAttribute('id',addItemId);
+    var img = document.createElement('img');
+    img.setAttribute('src',item.children[0].currentSrc);
+    var cartItems = document.getElementById('title');
+    cartItems.append(selectedItem);
+
+}
---
+++ product.js
@@ -0,0 +1,38 @@
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+$('#form1').submit(function(event){
+    // cancels the form submission
+    event.preventDefault();
+
+    //If the form is valid open modal
+    if($('#form1')[0].checkValidity() ){
+      $('#myModal').modal('toggle');
+    }
+  // do whatever you want here
+  });
